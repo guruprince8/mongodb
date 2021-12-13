73c07d90d0a261260766e7be90e91357453f705d
guruprince8 <47411355+guruprince8@users.noreply.github.com>
2021-12-13 23:51:44 +0530

    ini retrofitting

--- queries.sql
+++ queries.sql
@@ -1,4 +1,5 @@
 /* Create Admin User */
+/* https://dba.stackexchange.com/questions/111727/mongo-create-a-user-as-admin-for-any-database-raise-an-error */
 db.createUser(
   { 
     user: "mongoadmin" , 
